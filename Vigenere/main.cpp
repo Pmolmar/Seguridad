58af0184210e7c3ae35c38c2467eb3a530a12805
Pmolmar <alu0101103181@ull.edu.es>
2020-02-15 12:30:29 +0000

    Practica terminada

--- Vigenere/main.cpp
+++ Vigenere/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include <fstream>
 #include <vector>
+#include <algorithm>
 
 const int ALF_SIZE = 26;
+const int CASE = 65; //65 mayusculas, 75 minusculas
 
 void coder();
 void decoder();
@@ -53,33 +56,44 @@ int main(int argc, char const *argv[])
 
 void coder()
 {
-    std::string message;
-    std::string key;
+    std::ofstream file;
+    std::string message, key, name;
     std::vector<int> message_int, key_int, coded;
 
     std::cin.ignore();
 
-    std::cout << "Introduzca el mensaje:";
+    std::cout << "Introduzca el mensaje (MAYUSCULAS):";
     getline(std::cin, message);
+    message.erase(std::remove(message.begin(), message.end(), ' '), message.end());
 
-    std::cout << "Introduzca la key:";
+    std::cout << "Introduzca la key (MAYUSCULAS):";
     getline(std::cin, key);
 
-    //convertir a decimal, le resto 96 para que 'a'==1
+    //pedir ficheros de key y codificado, guardar la key
+    std::cout << "Introduzca fichero para la key: ";
+    getline(std::cin, name);
+    file.open(name);
+    file << key;
+    file.close();
+
+    std::cout << "Introduzca fichero para el mensaje codificado: ";
+    getline(std::cin, name);
+    file.open(name);
+
+    //convertir a decimal, le resto CASE para que 'A'==0 resot 97 para 'a'==0
     for (size_t i = 0; i < message.size(); i++)
     {
-        message_int.push_back(int(message[i]) - 97);
+        message_int.push_back(int(message[i]) - CASE);
     }
 
     for (size_t i = 0; i < key.size(); i++)
     {
-        key_int.push_back(int(key[i]) - 97);
+        key_int.push_back(int(key[i]) - CASE);
     }
 
     //realizar suma y el modulo del alfabeto
     unsigned int i = 0;
-    while ((i <= message_int.size() && i != key_int.size()) || 
-    (i <= key_int.size() && i != message_int.size()))
+    while (i < message_int.size() || i < key_int.size())
     {
         coded.push_back((message_int[i % message_int.size()] + key_int[i % key_int.size()]) % ALF_SIZE);
         ++i;
@@ -88,16 +102,53 @@ void coder()
     //convertir coded a caracter de nuevo
     for (size_t i = 0; i < coded.size(); i++)
     {
-        std::cout << char(coded[i] +97);
+        std::cout << char(coded[i] + CASE);
+        //guardar nuevo mensaje en fichero
+        file << char(coded[i] + CASE);
     }
-    std::cout<<std::endl;
-
-    //guardar nuevo mensaje en fichero
+    file.close();
+    std::cout << std::endl;
 }
 
 void decoder()
 {
+    std::ifstream file;
+    std::string coded, key, message, name;
+
+    std::cin.ignore();
+
     //pedir fichero de mensaje y llave
+    std::cout << "Introduzca fichero para la key: ";
+    getline(std::cin, name);
+    file.open(name);
+    file >> key;
+    file.close();
+
+    std::cout << key << std::endl;
+
+    std::cout << "Introduzca fichero para el mensaje: ";
+    getline(std::cin, name);
+    file.open(name);
+    file >> coded;
+    file.close();
+
+    std::cout << coded << std::endl;
 
     //descifrar y mostrar por pantalla
+    std::cout << "Mensaje descifrado: ";
+
+    int i = 0;
+    while (i < coded.size() || i < key.size())
+    {
+        int aux = (coded[i % coded.size()] - key[i % key.size()]);
+        if (aux < 0)
+            aux = aux + ALF_SIZE + CASE;
+        else
+            aux = aux + CASE;
+        
+        message.push_back(char(aux));
+        ++i;
+    }
+
+    std::cout << message << std::endl;
 }
