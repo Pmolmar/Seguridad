e815ea5dcb19803d2d2d98b21840d6a1fcfa35c1
Pablo Molina Martinez <alu0101103181@ull.edu.es>
2020-03-10 10:56:52 +0000

    A5 refactorizado

--- A5/main.cpp
+++ A5/main.cpp
@@ -4,58 +4,33 @@
 #include <vector>
 
 //---------------------------------------
-std::bitset<1> lsfr_1(std::bitset<19> &p)
+template <size_t bitsetsize>
+void lsfr(std::bitset<bitsetsize> &p)
 {
-    std::bitset<1> at = p[18] + p[17] + p[16] + p[13];
-      p<<=1;
-    p[0] = at[0];
-    return at;
-};
-std::bitset<1> lsfr_2(std::bitset<22> &p)
-{
-    std::bitset<1> at = p[21] + p[20];
-      p<<=1;
-    p[0] = at[0];
-    return at;
-};
-std::bitset<1> lsfr_3(std::bitset<23> &p)
-{
-    std::bitset<1> at = p[22] + p[21] + p[20] + p[7];
-    p<<=1;
-    p[0] = at[0];
-    return at;
-};
+    std::bitset<1> at;
 
+    if (p.size() == 19)
+        at = p[18] ^ p[17] ^ p[16] ^ p[13];
+    if (p.size() == 22)
+        at = p[21] ^ p[20];
+    if (p.size() == 23)
+        at = p[22] ^ p[21] ^ p[20] ^ p[7];
+
+    p <<= 1;
+    p[0] = at[0];
+};
 //---------------------------------------
-std::bitset<1> desplazamiento(std::bitset<19> &p1, std::bitset<22> &p2, std::bitset<23> &p3)
+void desplazamiento(std::bitset<19> &p1, std::bitset<22> &p2, std::bitset<23> &p3)
 {
-    std::bitset<1> a,b,c;
+    std::bitset<1> mayoria = p1[8] * p2[10] ^ p1[8] * p3[10] ^ p2[10] * p3[10];
 
-    if (p1[8] == p2[10] == p3[10])
-    {
-        a = lsfr_1(p1);
-        b = lsfr_2(p2);
-        c = lsfr_3(p3);
-    }
-    else if (p1[8] == p2[10])
-    {
-        a = lsfr_1(p1);
-        b = lsfr_2(p2);
-    }
-    else if (p1[8] == p3[10])
-    {
-        a = lsfr_1(p1);
-        c = lsfr_3(p3);
-    }
-    else
-    {
-        b = lsfr_2(p2);
-        c = lsfr_3(p3);
-    }
-
-    return(a^b^c);
+    if (p1[8] == mayoria[0])
+        lsfr(p1);
+    if (p2[10] == mayoria[0])
+        lsfr(p2);
+    if (p3[10] == mayoria[0])
+        lsfr(p3);
 };
-
 //---------------------------------------
 int main(int argc, char const *argv[])
 {
@@ -63,16 +38,27 @@ int main(int argc, char const *argv[])
     std::bitset<22> p2(std::string("0101100111100010011010"));
     std::bitset<23> p3(std::string("10111100110111100001111"));
 
-    std::cout << p1 << std::endl;
+    std::cout << "Original:" << std::endl
+              << p1 << std::endl
+              << p2 << std::endl
+              << p3 << std::endl
+              << std::endl;
 
-    //bucle 6 movimientos como en el pdf
+    std::bitset<6> x;
+
     for (int i = 0; i < 6; i++)
     {
-      std::cout << desplazamiento(p1, p2, p3);
+        x[6 - 1 - i] = p1[18] ^ p2[21] ^ p3[22];
+        if (i != 5)
+            desplazamiento(p1, p2, p3);
     }
+    std::cout << "Z:" << x << std::endl
+              << std::endl;
 
-    std::cout << std::endl;
-    std::cout << p1 << std::endl;
+    std::cout << "Desplazados:" << std::endl;
+    std::cout << p1 << std::endl
+              << p2 << std::endl
+              << p3 << std::endl;
 
     return 0;
 };
