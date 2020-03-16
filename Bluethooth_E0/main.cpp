a98828254451410d1549c3acdd100cc13ac41fad
Pmolmar <alu010110381@ull.edu.es>
2020-03-16 17:03:42 +0000

    Pruebas de funcioanmiento

--- Bluethooth_E0/main.cpp
+++ Bluethooth_E0/main.cpp
@@ -25,12 +25,12 @@ std::bitset<1> desplazamiento(std::bitset<25> &p1, std::bitset<31> &p2, std::bit
     //primero pasar inverso a una var R1/aux, normal R2
     std::bitset<2> R2 = R;
     R = R.flip();
-   
+
     int suma = int(p1[0] + p2[0] + p3[0] + p4[0]);
     suma += int(R.to_ulong());
 
-    std::bitset<1>z =p1[0] ^ p2[0] ^ p3[0] ^ p4[0] ^ R[0];
-    
+    std::bitset<1> z = p1[0] ^ p2[0] ^ p3[0] ^ p4[0] ^ R[0];
+
     lsfr(p1);
     lsfr(p2);
     lsfr(p3);
@@ -42,22 +42,23 @@ std::bitset<1> desplazamiento(std::bitset<25> &p1, std::bitset<31> &p2, std::bit
     T2[1] = R2[0] ^ R2[1];
     R2 = T2;
 
-    std::bitset<2> aux;
-    std::bitset<3> aux1 = suma;
-    aux[0] = aux1[1];
-    aux[1] = aux1[2];
-
+    std::bitset<2> aux = suma / 2;
     aux ^= R2;
     R ^= aux;
+
     return z;
 };
 
 int main(int argc, char const *argv[])
 {
-    std::bitset<25> reg_first(std::string("0111111111111111111111111"));                //p=8+12+20+25
-    std::bitset<31> reg_second(std::string("0111111111111111111111111111111"));         //p=12+16+24+31
-    std::bitset<33> reg_third(std::string("011111111111111111111111111111111"));        //p=4+24+28+33
-    std::bitset<39> reg_fourth(std::string("010101010101010101010101010101010101010")); //p=4+28+36+39
+    // std::bitset<25> reg_first(std::string("0111111111111111111111111"));                //p=8+12+20+25
+    // std::bitset<31> reg_second(std::string("0111111111111111111111111111111"));         //p=12+16+24+31
+    // std::bitset<33> reg_third(std::string("011111111111111111111111111111111"));        //p=4+24+28+33
+    // std::bitset<39> reg_fourth(std::string("010101010101010101010101010101010101010")); //p=4+28+36+39
+    std::bitset<25> reg_first(std::string("0100001111000000000001111"));                //p=8+12+20+25
+    std::bitset<31> reg_second(std::string("0111111111111111111111100011111"));         //p=12+16+24+31
+    std::bitset<33> reg_third(std::string("011111111111111111111111111011111"));        //p=4+24+28+33
+    std::bitset<39> reg_fourth(std::string("010000000000000000000000000000000101010")); //p=4+28+36+39
 
     std::bitset<2> R1(std::string("01"));
 
